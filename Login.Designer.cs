050ae73c0ae9d8134143209bb08db215ad442517
BloodWolf10 <126118338+BloodWolf10@users.noreply.github.com>
2023-06-03 22:44:56 -0500

    Login Form Created and Rendered

---
+++ Login.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SwiftShip_WindowApplication
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBoxAnimation = new System.Windows.Forms.PictureBox();
+            this.pBoxLogo = new System.Windows.Forms.PictureBox();
+            this.lblHello = new System.Windows.Forms.Label();
+            this.txtBxUsername = new System.Windows.Forms.TextBox();
+            this.txtBxPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxAnimation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBoxAnimation
+            // 
+            this.pBoxAnimation.Image = global::SwiftShip_WindowApplication.Properties.Resources.Cargo_Ship_Gif;
+            this.pBoxAnimation.Location = new System.Drawing.Point(0, -2);
+            this.pBoxAnimation.Name = "pBoxAnimation";
+            this.pBoxAnimation.Size = new System.Drawing.Size(400, 472);
+            this.pBoxAnimation.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxAnimation.TabIndex = 1;
+            this.pBoxAnimation.TabStop = false;
+            // 
+            // pBoxLogo
+            // 
+            this.pBoxLogo.Image = global::SwiftShip_WindowApplication.Properties.Resources.Swift_Ship_Logo_P;
+            this.pBoxLogo.Location = new System.Drawing.Point(509, 27);
+            this.pBoxLogo.Name = "pBoxLogo";
+            this.pBoxLogo.Size = new System.Drawing.Size(156, 128);
+            this.pBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBoxLogo.TabIndex = 0;
+            this.pBoxLogo.TabStop = false;
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.Font = new System.Drawing.Font("Passion of the Rose", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHello.Location = new System.Drawing.Point(500, 171);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(153, 49);
+            this.lblHello.TabIndex = 2;
+            this.lblHello.Text = "Ship Swift";
+            // 
+            // txtBxUsername
+            // 
+            this.txtBxUsername.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtBxUsername.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxUsername.Location = new System.Drawing.Point(481, 272);
+            this.txtBxUsername.Name = "txtBxUsername";
+            this.txtBxUsername.Size = new System.Drawing.Size(215, 29);
+            this.txtBxUsername.TabIndex = 3;
+            // 
+            // txtBxPassword
+            // 
+            this.txtBxPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtBxPassword.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxPassword.Location = new System.Drawing.Point(481, 339);
+            this.txtBxPassword.Name = "txtBxPassword";
+            this.txtBxPassword.PasswordChar = '*';
+            this.txtBxPassword.Size = new System.Drawing.Size(215, 29);
+            this.txtBxPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnLogin.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnLogin.Location = new System.Drawing.Point(610, 394);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(86, 42);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnExit.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnExit.Location = new System.Drawing.Point(491, 394);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(86, 42);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(481, 250);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(66, 19);
+            this.lblUsername.TabIndex = 7;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(481, 317);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 19);
+            this.lblPassword.TabIndex = 8;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Monotype Corsiva", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(506, 220);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(153, 13);
+            this.lblMessage.TabIndex = 9;
+            this.lblMessage.Text = "Managing your cargo the right way !";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(751, 467);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtBxPassword);
+            this.Controls.Add(this.txtBxUsername);
+            this.Controls.Add(this.lblHello);
+            this.Controls.Add(this.pBoxAnimation);
+            this.Controls.Add(this.pBoxLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxAnimation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBoxLogo;
+        private System.Windows.Forms.PictureBox pBoxAnimation;
+        private System.Windows.Forms.Label lblHello;
+        private System.Windows.Forms.TextBox txtBxUsername;
+        private System.Windows.Forms.TextBox txtBxPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
--- Login.cs
+++ Login.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace SwiftShip_WindowApplication
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
