3d029e89bb5c327c1d7e0f9e45e6b0ca3fda5592
Yungxristftp <126116534+Yungxristftp@users.noreply.github.com>
2023-06-24 11:09:42 -0500

    changes to inentory and vessel scheduling forms

--- InventoryManagment.Designer.cs
+++ InventoryManagment.Designer.cs
@@ -43,6 +43,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.TopPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LeftPBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
@@ -51,12 +53,13 @@
             // 
             // TopPanel
             // 
+            this.TopPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.TopPanel.Controls.Add(this.lblInventoryManagement);
-            this.TopPanel.Location = new System.Drawing.Point(454, 0);
+            this.TopPanel.Location = new System.Drawing.Point(556, 0);
             this.TopPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TopPanel.Name = "TopPanel";
-            this.TopPanel.Size = new System.Drawing.Size(548, 152);
+            this.TopPanel.Size = new System.Drawing.Size(696, 152);
             this.TopPanel.TabIndex = 2;
             // 
             // lblInventoryManagement
@@ -64,7 +67,7 @@
             this.lblInventoryManagement.AutoSize = true;
             this.lblInventoryManagement.Font = new System.Drawing.Font("Bahnschrift Condensed", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblInventoryManagement.ForeColor = System.Drawing.Color.White;
-            this.lblInventoryManagement.Location = new System.Drawing.Point(44, 45);
+            this.lblInventoryManagement.Location = new System.Drawing.Point(161, 43);
             this.lblInventoryManagement.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblInventoryManagement.Name = "lblInventoryManagement";
             this.lblInventoryManagement.Size = new System.Drawing.Size(454, 68);
@@ -73,20 +76,22 @@
             // 
             // LeftPBox
             // 
+            this.LeftPBox.Dock = System.Windows.Forms.DockStyle.Left;
             this.LeftPBox.Image = global::SwiftShip_WindowApplication.Properties.Resources.Cool_Ship;
             this.LeftPBox.Location = new System.Drawing.Point(0, 0);
             this.LeftPBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.LeftPBox.Name = "LeftPBox";
-            this.LeftPBox.Size = new System.Drawing.Size(462, 731);
+            this.LeftPBox.Size = new System.Drawing.Size(556, 823);
             this.LeftPBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LeftPBox.TabIndex = 3;
             this.LeftPBox.TabStop = false;
             // 
             // lbladditem
             // 
+            this.lbladditem.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lbladditem.AutoSize = true;
             this.lbladditem.Font = new System.Drawing.Font("Bahnschrift Condensed", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbladditem.Location = new System.Drawing.Point(663, 168);
+            this.lbladditem.Location = new System.Drawing.Point(828, 158);
             this.lbladditem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbladditem.Name = "lbladditem";
             this.lbladditem.Size = new System.Drawing.Size(138, 48);
@@ -95,9 +100,10 @@
             // 
             // lblitemname
             // 
+            this.lblitemname.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblitemname.AutoSize = true;
             this.lblitemname.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblitemname.Location = new System.Drawing.Point(476, 238);
+            this.lblitemname.Location = new System.Drawing.Point(655, 224);
             this.lblitemname.Name = "lblitemname";
             this.lblitemname.Size = new System.Drawing.Size(118, 34);
             this.lblitemname.TabIndex = 5;
@@ -105,16 +111,18 @@
             // 
             // txtbxItemname
             // 
-            this.txtbxItemname.Location = new System.Drawing.Point(598, 246);
+            this.txtbxItemname.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtbxItemname.Location = new System.Drawing.Point(777, 232);
             this.txtbxItemname.Name = "txtbxItemname";
             this.txtbxItemname.Size = new System.Drawing.Size(326, 26);
             this.txtbxItemname.TabIndex = 6;
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(476, 303);
+            this.label1.Location = new System.Drawing.Point(655, 289);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(108, 34);
             this.label1.TabIndex = 5;
@@ -122,16 +130,18 @@
             // 
             // txtBxItemType
             // 
-            this.txtBxItemType.Location = new System.Drawing.Point(598, 311);
+            this.txtBxItemType.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtBxItemType.Location = new System.Drawing.Point(777, 297);
             this.txtBxItemType.Name = "txtBxItemType";
             this.txtBxItemType.Size = new System.Drawing.Size(326, 26);
             this.txtBxItemType.TabIndex = 6;
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(488, 363);
+            this.label2.Location = new System.Drawing.Point(667, 349);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 34);
             this.label2.TabIndex = 5;
@@ -139,16 +149,18 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(598, 366);
+            this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.numericUpDown1.Location = new System.Drawing.Point(777, 352);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
             this.numericUpDown1.TabIndex = 7;
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(537, 438);
+            this.label3.Location = new System.Drawing.Point(716, 424);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 34);
             this.label3.TabIndex = 5;
@@ -156,9 +168,10 @@
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(694, 438);
+            this.label4.Location = new System.Drawing.Point(873, 424);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(104, 34);
             this.label4.TabIndex = 5;
@@ -166,9 +179,10 @@
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(850, 438);
+            this.label5.Location = new System.Drawing.Point(1029, 424);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(93, 34);
             this.label5.TabIndex = 5;
@@ -176,9 +190,10 @@
             // 
             // btnAdd
             // 
+            this.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnAdd.BackColor = System.Drawing.Color.SpringGreen;
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAdd.Location = new System.Drawing.Point(724, 363);
+            this.btnAdd.Location = new System.Drawing.Point(903, 349);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(80, 35);
             this.btnAdd.TabIndex = 8;
@@ -187,19 +202,50 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(543, 503);
+            this.dataGridView1.Location = new System.Drawing.Point(722, 489);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(400, 214);
             this.dataGridView1.TabIndex = 9;
             // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnBack.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnBack.Location = new System.Drawing.Point(926, 746);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(129, 65);
+            this.btnBack.TabIndex = 10;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSubmit.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSubmit.Location = new System.Drawing.Point(1082, 746);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(129, 65);
+            this.btnSubmit.TabIndex = 11;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            // 
             // InventoryManagment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(970, 729);
+            this.ClientSize = new System.Drawing.Size(1259, 823);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.numericUpDown1);
@@ -243,5 +289,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
--- VesselScheduling.Designer.cs
+++ VesselScheduling.Designer.cs
@@ -32,12 +32,14 @@
             this.lblUpdateAccount = new System.Windows.Forms.Label();
             this.LeftPBox = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtbxVesselName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtbxEstimatedTime = new System.Windows.Forms.TextBox();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
             this.UpdateBackPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LeftPBox)).BeginInit();
             this.SuspendLayout();
@@ -49,7 +51,7 @@
             this.UpdateBackPanel.Location = new System.Drawing.Point(505, 0);
             this.UpdateBackPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.UpdateBackPanel.Name = "UpdateBackPanel";
-            this.UpdateBackPanel.Size = new System.Drawing.Size(520, 142);
+            this.UpdateBackPanel.Size = new System.Drawing.Size(755, 142);
             this.UpdateBackPanel.TabIndex = 2;
             // 
             // lblUpdateAccount
@@ -57,7 +59,7 @@
             this.lblUpdateAccount.AutoSize = true;
             this.lblUpdateAccount.Font = new System.Drawing.Font("Bahnschrift Condensed", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUpdateAccount.ForeColor = System.Drawing.Color.White;
-            this.lblUpdateAccount.Location = new System.Drawing.Point(108, 42);
+            this.lblUpdateAccount.Location = new System.Drawing.Point(227, 37);
             this.lblUpdateAccount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUpdateAccount.Name = "lblUpdateAccount";
             this.lblUpdateAccount.Size = new System.Drawing.Size(363, 68);
@@ -79,24 +81,24 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(568, 432);
+            this.label1.Location = new System.Drawing.Point(640, 468);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(117, 29);
             this.label1.TabIndex = 5;
             this.label1.Text = "Vessel Name:";
             // 
-            // textBox1
+            // txtbxVesselName
             // 
-            this.textBox1.Location = new System.Drawing.Point(691, 434);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(275, 26);
-            this.textBox1.TabIndex = 6;
+            this.txtbxVesselName.Location = new System.Drawing.Point(763, 470);
+            this.txtbxVesselName.Name = "txtbxVesselName";
+            this.txtbxVesselName.Size = new System.Drawing.Size(275, 26);
+            this.txtbxVesselName.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(548, 505);
+            this.label2.Location = new System.Drawing.Point(620, 541);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(137, 29);
             this.label2.TabIndex = 5;
@@ -104,7 +106,7 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(691, 508);
+            this.dateTimePicker1.Location = new System.Drawing.Point(763, 544);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(266, 26);
             this.dateTimePicker1.TabIndex = 7;
@@ -113,34 +115,62 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(547, 568);
+            this.label3.Location = new System.Drawing.Point(619, 604);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(138, 29);
             this.label3.TabIndex = 5;
             this.label3.Text = "Estimated Time:";
             // 
-            // textBox2
+            // txtbxEstimatedTime
             // 
-            this.textBox2.Location = new System.Drawing.Point(691, 573);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(275, 26);
-            this.textBox2.TabIndex = 6;
+            this.txtbxEstimatedTime.Location = new System.Drawing.Point(763, 609);
+            this.txtbxEstimatedTime.Name = "txtbxEstimatedTime";
+            this.txtbxEstimatedTime.Size = new System.Drawing.Size(275, 26);
+            this.txtbxEstimatedTime.TabIndex = 6;
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(625, 156);
+            this.monthCalendar1.Location = new System.Drawing.Point(733, 169);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 8;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSubmit.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSubmit.Location = new System.Drawing.Point(1095, 741);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(129, 65);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnBack.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnBack.Location = new System.Drawing.Point(939, 741);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(129, 65);
+            this.btnBack.TabIndex = 9;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
             // 
             // VesselScheduling
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1024, 618);
+            this.ClientSize = new System.Drawing.Size(1259, 863);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtbxEstimatedTime);
+            this.Controls.Add(this.txtbxVesselName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
@@ -162,11 +192,13 @@
         private System.Windows.Forms.Label lblUpdateAccount;
         private System.Windows.Forms.PictureBox LeftPBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtbxVesselName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtbxEstimatedTime;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnBack;
     }
 }
